f6d259b5390a723f60a1dd85eb556414f0b8b111
SajinTamang <sajin.god1.sl@gmail.com>
2021-12-30 21:26:06 +0800

    commit on 'Review Application'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
+import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
+import Card from "./components/shared/Card";
+
 function App(){
-    return  <h1>Hello from the Review app</h1>
-    
+const [feedback, setFeedback] = useState(FeedbackData)
+    return (
+      <>
+        <Header />
+
+        <div className="container">
+             <FeedbackList feedback={feedback}/>
+             
+        </div>
+      </>
+    );
 }
 
 
