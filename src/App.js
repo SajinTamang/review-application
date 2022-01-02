ef81370da8bd62417bef3cb893222663f8c5590a
SajinTamang <sajin.god1.sl@gmail.com>
2022-01-02 22:23:34 +0800

    commit on "Review-Application"

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from "react";
 import { useState } from "react";
 
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
-import Card from "./components/shared/Card";
+
 
 function App(){
-const [feedback, setFeedback] = useState(FeedbackData)
+const [feedback, setFeedback] = useState(FeedbackData);
+
+
+
+const deleteFeedback = (id) =>{
+  if(window.confirm("Are you sure you want to delete?")){
+    setFeedback(feedback.filter((item)=> item.id !== id ));
+  }
+}
     return (
       <>
         <Header />
 
         <div className="container">
-             <FeedbackList feedback={feedback}/>
-             
+          <FeedbackForm />
+          <FeedbackStats feedback={feedback} />
+          <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
         </div>
       </>
     );
--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -6,12 +6,12 @@ const FeedbackData = [
   },
   {
     id: 2,
-    rating: 2,
+    rating: 10,
     text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde excepturi, odio aliquam dolorem quidem, dolores voluptatum sint fugiat ut ad nihil possimus cum laudantium, exercitationem quas tempore. Sit, repellat repudiandae!",
   },
   {
     id: 3,
-    rating: 4,
+    rating: 3,
     text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde excepturi, odio aliquam dolorem quidem, dolores voluptatum sint fugiat ut ad nihil possimus cum laudantium, exercitationem quas tempore. Sit, repellat repudiandae!",
   },
 ];
